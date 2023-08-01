47b3cbb684661f8e5e166a5b8b6b5298a4357cdb
Jared <noemail>
2023-08-01 15:46:28 -0400

    Updating dan's script on each build.

--- script.sh
+++ script.sh
@@ -1,3 +1,7 @@
 #!/usr/bin/env sh
-#
+# example of scrip to be executed on packer launch
 echo "hello world" > /home/ubuntu/packer.test
+
+# Pull Dan's latest script  on rebuilds
+cd /home/ubuntu/pve-consumer-deployment;
+git stash && git pull;
